64c9ed173e5d7b7c9e2946ce5079132bff73b5da
tianzhen <tianzhen@yonyou.com>
2019-09-03 16:27:47 +0800

    create project

---
+++ admin/src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from './views/Main'
+import CategoryEdit from './views/CategoryEdit'
+import CategoryList from './views/CategoryList'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'main',
+      component: Main,
+      children:[
+        {
+          path: 'categories/create',
+          component: CategoryEdit,
+        },
+        {
+          path: 'categories/list',
+          component: CategoryList,
+        }
+      ]
+    },
+  ]
+})
---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        新建分类
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
