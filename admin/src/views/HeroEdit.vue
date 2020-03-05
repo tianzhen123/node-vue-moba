d7b5c61a8fce55cf5619286024b56d9fab6c91d4
tianzhen <18811128977@163.com>
2020-03-05 17:42:42 +0800

    修改其他上传配置

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -75,7 +75,8 @@
               <el-form-item label="图标">
                 <el-upload
                   class="avatar-uploader"
-                  :action="$http.defaults.baseURL +'/upload'"
+                  :action="uploadUrl"
+                  :headers="getAuthHeaders()"
                   :show-file-list="false"
                   :on-success="res => $set(item,'icon',res.url)"
                 >
@@ -91,9 +92,7 @@
               </el-form-item>
               <el-form-item>
                 <el-button type="danger" size="small" @click="model.skills.splice(i, 1)">删除</el-button>
-                
               </el-form-item>
-              
             </el-col>
           </el-row>
         </el-tab-pane>
@@ -165,5 +164,4 @@ export default {
 };
 </script>
 <style>
-
 </style>
